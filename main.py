daf32c32a8017cebe84bebb981e216eb6fbfb2cc
Ashley-Wrrk <Ashley.Wrrk@gmail.com>
2026-04-22 16:47:48 +1200

    Update main.py
    added twilio/weather info

--- main.py
+++ main.py
@@ -11,10 +11,16 @@ import pandas
 import random
 import smtplib
 import os
+import requests
+from twilio.rest import Client
 
 # import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
+
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
@@ -36,3 +42,34 @@ if today_tuple in birthdays_dict:
             to_addrs=birthday_person["email"],
             msg=f"Subject:Happy Birthday!\n\n{contents}"
         )
+
+### === ###
+
+weather_params = {
+    "key": api_key,
+    "q": "-37.787003,175.279251",
+    "days":1,
+}
+
+response = requests.get(WeatherAPI_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+rain_code = [1063, 1072, 1150, 1153, 1168, 1171, 1180, 1183, 1186, 1189, 1192, 1195, 1198, 1201, 1240,
+             1243, 1246, 1273, 1276]
+new_list = [1063, 1240]
+
+will_rain = False
+for hour_data in weather_data["forecast"]["forecastday"][0]["hour"]:
+    condition_code = hour_data["condition"]["code"]
+
+    if condition_code in rain_code:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_="+17755263506",
+        to="+18777804236"
+    )
+print(message.status)
